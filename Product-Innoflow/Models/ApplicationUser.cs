3bdbbf60eab7a5d7374dca2ec54afb1ea8e78e2f
Anton <zditovets55@gmail.com>
2020-10-10 18:14:11 +0300

    1

---
+++ Product-Innoflow/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product_Innoflow.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
